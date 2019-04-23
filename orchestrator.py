5f6818685b61f45a1dce29b57e4919ff599512be
Aleix Sancho <38431914+aleixsancho@users.noreply.github.com>
2019-04-23 22:12:59 +0200

    Implementació amb cues

--- orchestrator.py
+++ orchestrator.py
@@ -7,8 +7,14 @@ import yaml
 import sys
 import re
 import time
+import pika
 from COS_backend import COS_backend
 from ibm_cf_connector import CloudFunctions
+
+def callback(ch, method, properties, body):
+    message = body.decode('latin1')
+    if message == "stop":
+        channel.stop_consuming()
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
@@ -17,6 +23,7 @@ if __name__ == '__main__':
     
         config_file = res['ibm_cos']
         config_file_cloud = res['ibm_cf']
+        amqp_url = res['queue_sd']
         invocador = CloudFunctions(config_file_cloud)
         cos_backend = COS_backend(config_file)
         
@@ -37,11 +44,6 @@ if __name__ == '__main__':
         for elem in ll:
             file = elem["Key"]
             cos_backend.delete_object("sanchoericbucket", file)
-        
-        with open('count_words.zip', 'rb') as count_words:
-            codi = count_words.read()
-            
-        invocador.create_action('count_words', codi)
         
         with open('reducer.zip', 'rb') as reducer:
             codi = reducer.read()
@@ -78,47 +80,33 @@ if __name__ == '__main__':
             
             text = cos_backend.get_object('sanchoericbucket', sys.argv[1], extra_get_args = {'Range': 'bytes=' + str(inici_bytes) + '-' + str(final_bytes)})
             decoded_text = text.decode("latin1")
-            params = {'config_file':config_file, 'text':decoded_text, 'index':index}
+            params = {'config_file':config_file, 'text':decoded_text, 'index':index, 'amqp':amqp_url}
             invocador.invoke('word_count', params)
             index = index + 1
             inici_bytes = int((tamany_particionat * index) + offset)
-        
-        tots_acabats = False
-        
-        while not tots_acabats:
-            llista_objectes = cos_backend.list_objects('sanchoericbucket', 'word_count')
-            if len(llista_objectes) == num_workers:
-                tots_acabats = True
-    
-        params = {'config_file':config_file}
+
+        params = {'config_file':config_file, 'amqp':amqp_url, 'num_workers':num_workers}
         invocador.invoke('reducer', params)
-        tots_acabats = False
+        url = amqp_url.get("amqp")
+        params = pika.URLParameters(url)
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.queue_declare(queue='message_queue')
+        channel.basic_consume(callback, queue='message_queue', no_ack=True)
+        channel.start_consuming()
         
-        while not tots_acabats:
-            llista_objectes = cos_backend.list_objects('sanchoericbucket', 'reducer')
-            if len(llista_objectes) == 1:
-                tots_acabats = True
-        
-        invocador.invoke('count_words', params)
-        tots_acabats = False
-        
-        while not tots_acabats:
-            llista_objectes = cos_backend.list_objects('sanchoericbucket', 'count_word')
-            if len(llista_objectes) == 1:
-                tots_acabats = True
-                
         final_time = time.time()
         total_time = final_time - initial_time
         print(total_time)
-        objecte_count_words = cos_backend.get_object('sanchoericbucket', 'count_words.txt')
+        objecte_count_words = cos_backend.get_object('sanchoericbucket', 'count_words_amqp.txt')
         objecte_decoded_count_words = objecte_count_words.decode('utf-8')
-        objecte_reducer = cos_backend.get_object('sanchoericbucket', 'reducer.txt')
+        objecte_reducer = cos_backend.get_object('sanchoericbucket', 'reducer_amqp.txt')
         objecte_decoded_reducer = objecte_reducer.decode('utf-8')
-        f1 = open('resultat_count_words.txt', 'w+')
+        f1 = open('resultat_count_words_amqp.txt', 'w+')
         f1.write(objecte_decoded_count_words)
-        f2 = open('resultat_reducer.txt', 'w+')
+        f2 = open('resultat_reducer_amqp.txt', 'w+')
         f2.write(objecte_decoded_reducer)
         f1.close()
         f2.close()
     else:
-        print("Necessites introduir tres parametres")
+        print("Necessites introduir dos parametres")
